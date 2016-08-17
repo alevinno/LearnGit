7a42512d5a6bf3704e7faa3cdf986eb47ef23bea
家振 王 <alevinno@qq.com>
2016-08-17 19:40:49 +0800

    add object c code

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReactDemo01
+//
+//  Created by 家振 王 on 16/8/3.
+//  Copyright © 2016年 家振 王. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
